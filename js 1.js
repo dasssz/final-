122aa5f3f4d57b82eac907761c88c5489ac48115
dasssz <145177246+dasssz@users.noreply.github.com>
2024-01-07 20:30:15 +0800

    Add files via upload

---
+++ js 1.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+function sendMoney() {
+    var budget1 = parseInt(document.querySelector('#budget1').textContent);
+    var budget2 = parseInt(document.querySelector('#budget2').textContent);
+
+    if (budget1 >= 5) {
+        budget1 -= 5;
+        budget2 += 5;
+    } else {
+        alert("Insufficient budget on First Website.");
+    }
+
+    document.querySelector('#budget1').textContent = budget1;
+    document.querySelector('#budget2').textContent = budget2;
+}
+
+function sendMoney() {
+    
+}
+});
